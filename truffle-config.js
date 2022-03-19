6c94a9b3bf4ee533eed65c4e4269f0d835ab9fad
Richard Kovacs <richardkovacs.it@gmail.com>
2022-03-19 14:33:44 +0100

    ci(ropsten): add ropsten deployment

--- truffle-config.js
+++ truffle-config.js
@@ -65,7 +65,8 @@ module.exports = {
         mnemonic,
         `https://ropsten.infura.io/v3/${infuraProjectId}`),
       network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gas: 6721975,        // Ropsten has a lower block limit than mainnet
+      gasPrice: 20000000000,
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
@@ -95,13 +96,13 @@ module.exports = {
     solc: {
       version: "^0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 1000
+        },
+        evmVersion: "homestead"
+      }
     }
   },
 
